89243e5ffb9533350a1c939b93bd0ba8e0873e2f
Wes Freeman <wes@skeweredrook.com>
2014-06-27 01:46:45 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -5,9 +5,9 @@ LazyLoad.js(['js/lib/jquery-1.10.2.min.js', 'bootstrap/js/bootstrap.min.js', 'js
 	var pckry;
 	imagesLoaded( container, function() {
 		pckry = new Packery( container, {
-			columnWidth: '.col-md-4',
+			columnWidth: '.col-xs-3',
 			itemSelector: '.item',
-			gutter: 0  
+			gutter: 10  
 		});
 	});
 	// TICKER
